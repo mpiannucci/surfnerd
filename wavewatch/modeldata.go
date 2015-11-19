9bc59fee52cdf3ceb9409ea6744e589456cedf17
miannucci <miannucci@navatekltd.com>
2015-11-19 09:36:00 -0500

    Add data map getter for non arm targets

--- wavewatch/modeldata.go
+++ wavewatch/modeldata.go
@@ -3,6 +3,27 @@
 package wavewatch
 
 type ModelData struct {
-	Location *Location
+	*Location
+	ModelRun string
 	Data     modelDataMap
+}
+
+func FetchWaveWatchDataMap(loc *Location) *ModelData {
+
+	eastCoastModel := EastCoastModel{}
+	if !eastCoastModel.ContainsLocation(loc) {
+		return nil
+	}
+
+	// Fetch the raw data
+	modelTime, _ := latestModelDateTime()
+	rawData, err := fetchRawWaveWatchData(loc, &eastCoastModel, &modelTime)
+	if err != nil {
+		return nil
+	}
+
+	// Call to parse the raw data into containers
+	modelDataContainer := parseRawWaveWatchData(rawData)
+	modelData := &ModelData{loc, formatViewingTime(modelTime), modelDataContainer}
+	return modelData
 }
