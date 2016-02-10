e8b43fb3821243bc0b8a9b5bd2fe10afa2d5b41a
miannucci <miannucci@navatekltd.com>
2016-02-10 13:44:17 -0500

    Working refraction, shoaling, and breaking solvers

--- wavetools.go
+++ wavetools.go
@@ -45,3 +45,62 @@ func LDis(period, depth float64) float64 {
 
 	return 2 * math.Pi * depth / Xf
 }
+
+// Solves for the Breaking Wave Height and Breaking Water Depth given a swell and beach conditions.
+// All units are metric and gravity is 9.81.
+func SolveBreakingCharacteristics(period, incidentAngle, deepWaveHeight, beachSlope, waterDepth float64) (breakingWaveHeight, breakingWaterDepth float64) {
+	const gravity = 9.81
+	incidentAngleRad := incidentAngle * math.Pi / 180
+
+	// Find all of the wave characteristics
+	wavelength := LDis(period, waterDepth)
+
+	deepWavelength := (gravity * math.Pow(period, 2)) / (2 * math.Pi)
+	initialCelerity := (gravity * period) / (2 * math.Pi)
+	celerity := wavelength / period
+	theta := math.Asin(celerity * ((math.Sin(incidentAngleRad)) / initialCelerity))
+	refractionCoeff := math.Sqrt(math.Cos(incidentAngleRad) / math.Cos(theta))
+	a := 43.8 * (1 - math.Exp(-19*beachSlope))
+	b := 1.56 / (1 + math.Exp(-19.5*beachSlope))
+	deepRefractedWaveHeight := refractionCoeff * deepWaveHeight
+	w := 0.56 * math.Pow(deepRefractedWaveHeight/deepWavelength, -0.2)
+
+	// Find the breaking wave height!
+	breakingWaveHeight = w * deepRefractedWaveHeight
+
+	// Solve for the breaking depth
+	K := b - a*(breakingWaveHeight/(gravity*math.Pow(period, 2)))
+	breakingWaterDepth = breakingWaveHeight / K
+
+	return
+}
+
+// Calculate the refraction coefficient Kr with given
+// inputs on a straight beach with parrellel bottom contours
+func SolveRefractionCoefficient(wavelength, depth, incidentAngle float64) (refractionCoeff, shallowIncidentAngle float64) {
+	incidentAngleRad := incidentAngle * math.Pi / 180.0
+	wavenumber := (2.0 * math.Pi) / wavelength
+	shallowIncidentAngleRad := math.Asin(math.Sin(incidentAngleRad) * math.Tanh(wavenumber*depth))
+	refractionCoeff = math.Sqrt(math.Cos(incidentAngleRad) / math.Cos(shallowIncidentAngleRad))
+	shallowIncidentAngle = shallowIncidentAngleRad * 180 / math.Pi
+	return
+}
+
+// Calculate the shoaling coeffecient Ks. Units are metric, gravity is 9.81
+func SolveShoalingCoefficient(wavelength, depth float64) (shoalingCoefficient float64) {
+	const gravity = 9.81
+
+	// Basic dispersion relationships
+	wavenumber := (2.0 * math.Pi) / wavelength
+	deepWavelength := wavelength / math.Tanh(wavenumber*depth)
+	w := math.Sqrt(wavenumber * gravity)
+	period := (2.0 * math.Pi) / w
+
+	// Celerity
+	initialCelerity := deepWavelength / period
+	celerity := initialCelerity * math.Tanh(wavenumber*depth)
+	groupVelocity := 0.5 * celerity * (1 + ((2 * wavenumber * depth) / (math.Sinh(2 * wavenumber * depth))))
+
+	shoalingCoefficient = math.Sqrt(initialCelerity / (2 * groupVelocity))
+	return
+}
--- wavetools_test.go
+++ wavetools_test.go
@@ -16,3 +16,29 @@ func TestLDis(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestBreakSolver(t *testing.T) {
+	// 4 feet at 10 secs, no incident angle
+	breakingHeight, breakingDepth := SolveBreakingCharacteristics(10.0, 0, 1.219, 0.01, 30.0)
+	if math.Abs(breakingHeight-1.8017) > 0.0001 {
+		t.Fail()
+	} else if math.Abs(breakingDepth-2.1401) > 0.0001 {
+		t.Fail()
+	}
+}
+
+func TestRefractionSolver(t *testing.T) {
+	Kr, theta := SolveRefractionCoefficient(150.0, 10.0, 30.0)
+	if math.Abs(Kr-0.93996) > 0.0001 {
+		t.Fail()
+	} else if math.Abs(theta-11.42) > 0.001 {
+		t.FailNow()
+	}
+}
+
+func testShoalingSolver(t *testing.T) {
+	Ks := SolveShoalingCoefficient(150.0, 10.0)
+	if math.Abs(Ks-1.1553) > 0.0001 {
+		t.Fail()
+	}
+}
