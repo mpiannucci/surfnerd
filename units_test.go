152584946df163931cca8c13453f7b1358fa8deb
miannucci <miannucci@navatekltd.com>
2016-02-16 10:11:08 -0500

    Add simple degree unit testing

--- units_test.go
+++ units_test.go
@@ -1,6 +1,7 @@
 package surfnerd
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -19,6 +20,24 @@ func TestTimeConversion(t *testing.T) {
 	}
 
 	if ToTwentyFourHourFormat("12 am") != "00z" {
+		t.Fail()
+	}
+}
+
+func TestDegreeConversion(t *testing.T) {
+	if DegreeToDirection(360) != "N" {
+		t.Fail()
+	}
+
+	if DegreeToDirection(4) != "N" {
+		t.Fail()
+	}
+
+	if DegreeToDirection(198) != "SSW" {
+		t.Fail()
+	}
+
+	if DegreeToDirection(208) != "SSW" {
 		t.Fail()
 	}
 }
